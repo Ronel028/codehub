7affa3cdbe4fdafab11b442327136b87fc288167
roneldev <floridaronel15@gmail.com>
2024-06-20 19:29:48 -0700

    hide category list if no data

--- resources/js/Pages/Home/Blogs.jsx
+++ resources/js/Pages/Home/Blogs.jsx
@@ -65,15 +65,19 @@ const Blogs = (props) => {
                     )
                 }    
 
-                <div className="mb-10 flex items-center gap-2">
-                    {
-                        props.categories.map(category => (
-                            <Link href={`/blog-list/blog/${category.name.toLowerCase()}`} className="flex items-center shadow gap-2 text-xs border border-[#415a77] hover:bg-[#415a77] text-light py-2 px-3 rounded-md">
-                                {category.name}
-                            </Link>
-                        ))
-                    }
-                </div>       
+                {
+                    props.latest_blog.length > 0 ? (
+                        <div className="mb-10 flex items-center gap-2">
+                            {
+                                props.categories.map(category => (
+                                    <Link href={`/blog-list/blog/${category.name.toLowerCase()}`} className="flex items-center shadow gap-2 text-xs border border-[#415a77] hover:bg-[#415a77] text-light py-2 px-3 rounded-md">
+                                        {category.name}
+                                    </Link>
+                                ))
+                            }
+                        </div>       
+                    ) : null
+                }
                 
                 {/* TECHNOLOGY BLOG */}
                 {
