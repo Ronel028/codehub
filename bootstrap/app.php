2bf4af128ffd80ec3cdb4a41ff05221e32546f3c
Ronel028 <floridaronel15@gmail.com>
2025-01-29 19:10:15 +0800

    modify middleware header 1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            InertiaCorsMiddleware::class,
             HandleInertiaRequests::class,
+            InertiaCorsMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
