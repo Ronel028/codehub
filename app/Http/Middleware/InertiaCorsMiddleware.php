3cd2e94cb5ccc57100d1767b0939a1817596796a
Ronel028 <floridaronel15@gmail.com>
2025-01-29 19:06:43 +0800

    modify middleware header

--- app/Http/Middleware/InertiaCorsMiddleware.php
+++ app/Http/Middleware/InertiaCorsMiddleware.php
@@ -19,12 +19,7 @@ class InertiaCorsMiddleware
 
         // Ensure the response is modifiable
         if ($response instanceof Response) {
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'X-Inertia, Content-Type, X-Requested-With');
             $response->headers->set('Access-Control-Expose-Headers', 'X-Inertia');
-            $response->headers->set('Vary', 'Origin');
-            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
         }
 
         return $response;
